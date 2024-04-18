897897d4a9ff1363a99ca439d97c312e207dcfda
Richard Hadzhiev <ri4koto100@gmail.com>
2024-04-18 16:33:07 +0200

    Solved lab

--- src/index.js
+++ src/index.js
@@ -7,16 +7,40 @@ let timer = null; // Variable to store the interval
 // ITERATION 1: Add event listener to the start button
 
 // Your code goes here ...
-
-
+const startBtn = document.getElementById("start-btn")
+startBtn.addEventListener("click", () => {
+  startCountdown(10)
+})
 
 
 // ITERATION 2: Start Countdown
-function startCountdown() {
+function startCountdown(timer) {
   console.log("startCountdown called!");
 
 
   // Your code goes here ...
+  startBtn.disabled = true
+  let remainingTime = timer
+  updateTimerDisplay(remainingTime);
+  const interval = setInterval(() => {
+    remainingTime --;
+    
+  
+    updateTimerDisplay(remainingTime);
+    if(remainingTime === 0){
+      clearInterval(interval)
+      startBtn.disabled = false;
+      showToast();
+      
+    }
+    
+  },1000)
+  
+}
+function updateTimerDisplay(time){
+  const formattedTime = document.getElementById("time");
+formattedTime.textContent = `${time}`
+
 }
 
 
@@ -28,7 +52,14 @@ function showToast(message) {
 
   // Your code goes here ...
 
-
+const toastId = document.getElementById("toast")
+// const toastMsg = document.getElementById("toast-message")
+// toastMsg.textContent = message;
+toastId.classList.add("show");
+setTimeout(() => {
+  toastId.classList.remove("show")
+  // toastMsg.textContent = message
+},3000)
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
